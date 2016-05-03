215661f1cc6b0262f07dfd6edfa3718a50e29c45
Stephen Beckett <beckett.stephenj@gmail.com>
2016-05-03 18:33:56 -0400

    Added files via upload

---
+++ model_analysis/array2vstruct.m
@@ -0,0 +1,17 @@
+function s = array2vstruct(x_array,fnames)
+% function s = array2vstruct(x_array,fnames)
+% This is a function which takes an array of values
+% and places them in order into a particular structure
+% in the order of the fieldnames in fnames.
+
+if (length(fnames)~=length(x_array))
+  error('Wrong number of elements to unpack in array2vstruct');
+  exit(-1);
+else
+  tmpc=cell(1,length(x_array));
+  for i=1:length(x_array),
+    tmpc{i}=x_array(i);
+  end
+  s = cell2struct(tmpc,fnames,2);
+end
+end
---
+++ model_analysis/psprintc.m
@@ -0,0 +1,13 @@
+function psprintc(filename)
+% psprint('filename')
+% prints current figure with 
+% the following flags
+% -depsc2
+% .ps will be appended
+%
+% print in color
+filenameps = sprintf('%s.ps',filename);
+print(filenameps,'-depsc2');
+filenamjpg = sprintf('%s.jpg',filename);
+print(filenamjpg,'-djpeg80');
+end
